df51fbfd663a87202803597ac63145eb67ab197f
biencuongta <158592866+biencuongta@users.noreply.github.com>
2024-02-02 23:29:42 +0700

    declare program address

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("BEf7PvxMnnvML8jPP7egkctjzEgrDGSpn8GCEkpRNE1k");
 
 #[program]
 pub mod nft_lend_borrow {
